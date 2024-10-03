35eddeeda65f63cd226acaae3c42d5619f886a00
ssadown <bibatututu@yandex.ru>
2024-10-03 23:15:06 +0500

    Настроил страницу not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,33 +1,20 @@
-"use client"
-
 import React from 'react';
 import styles from "./not-found.module.css"
 import image from "../../public/assets/404.png"
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import Button from '@/Components/Common/Button/Button';
+import Link from 'next/link';
 
 const NotFound = () => {
 
-    const {replace} = useRouter()
-
-    const handleReplace = () => {
-        replace('/')
-    }
-
     return (
-        <main className={`main ${styles.content}`}>
+        <main className={`${styles.content}`}>
             <Image
-            height={100}
-            width={100}
+            height={300}
+            width={300}
             src={image}
             alt='Страница не найдена'
             />
-            <Button
-                text='Вернуться на главную страницу'
-                size='l'
-                onClick={handleReplace}
-            />
+            <Link href={'/'} className={`${styles.link}`}>Вернуться назад</Link>
         </main>
     );
 }
