988e983cafc86b5a0537f6a5c50995c12b5aa6fd
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-07 23:31:15 +0500

    Добавил пару иконок в header, оптимизировал верстку
    А еще поменял svgшки и теперь их можно нормально анимировать (ура)

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import styles from './Footer.module.css'
-import Image from 'next/image';
-import logo from "../../../public/assets/LogoTiu.jpg"
+import React from "react";
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import logo from "../../../public/assets/LogoTiu.jpg";
 
 const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            <Image
-                width={80}
-                height={80}
-                className={styles.image}
-                src={logo}
-                alt="Логотип ВШЦТ"
-            />
-            <h1 className={styles.text__header}>Отметка</h1>
-        </div>
-    );
-}
+  return (
+    <footer className={styles.footer}>
+      <Image
+        width={80}
+        height={80}
+        className={styles.image}
+        src={logo}
+        alt="Логотип ВШЦТ"
+        priority={true}
+      />
+      <h1 className={styles.text__header}>Отметка</h1>
+    </footer>
+  );
+};
 
 export default Footer;
