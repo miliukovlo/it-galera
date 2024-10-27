6809be8bc9b9fa43900fc99e1b2ebd4ed67c7fb0
ssadown <bibatututu@yandex.ru>
2024-10-27 20:33:59 +0500

    Переделал модальное окно

--- src/Components/Users/[id]/LessonsList/LessonsList.tsx
+++ src/Components/Users/[id]/LessonsList/LessonsList.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styles from './LessonsList.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './LessonsList.module.css';
 import UserModal from '../../UserModal/UserModal';
 import { lessonsInterface } from '@/Interface/generatedStudentsInterface';
 import Circle from '@/Components/Common/Circle/Circle';
@@ -8,50 +8,58 @@ interface LessonsListProps {
     lessons?: lessonsInterface[]
 }
 
-const LessonsList : React.FC<LessonsListProps> = ({
-    lessons
-}: LessonsListProps) => {
+const LessonsList: React.FC<LessonsListProps> = ({ lessons }: LessonsListProps) => {
 
     interface modalInterface {
-		item: string;
-		isOpen: boolean;
-	}
+        item: string;
+        isOpen: boolean;
+    }
 
-	const [modal, setModal] = useState<modalInterface>({
-		item: "",
-		isOpen: false,
-	});
+    const [modal, setModal] = useState<modalInterface>({
+        item: "",
+        isOpen: false,
+    });
+
+    useEffect(() => {
+        if (modal.isOpen) {
+            document.body.style.height = "100vh"
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.height = ""
+            document.body.style.overflow = ""; 
+        }
+    }, [modal.isOpen]);
 
     return (
         <div className={styles.lessons__block}>
-				<UserModal
-					subject_name={modal.item}
-					isOpen={modal.isOpen}
-					onClose={() => {
-						setModal(prevModal => ({ ...prevModal, isOpen: false }));
-					}}
-				/>
-				{lessons?.map(lesson => {
-					return (
-						<div
-							onClick={() => {
-								setModal(() => ({ isOpen: true, item: lesson.name }));
-							}}
-							className={styles.lesson}
-							key={lesson.name}>
-							<p className={styles.information__text}>
-								<b>Предмет:</b> {lesson.name}
-							</p>
-							<div className={styles.progress__block}>
-								<p className={styles.progress__text}>
-									<b>Посещение:</b>
-								</p>
-								<Circle progress={lesson.attendance} />
-							</div>
-						</div>
-					);
-				})}
-			</div>
+            <UserModal
+                subject_name={modal.item}
+                isOpen={modal.isOpen}
+                onClose={() => {
+                    setModal(prevModal => ({ ...prevModal, isOpen: false }));
+                }}
+            />
+            {lessons?.map(lesson => {
+                return (
+                    <div
+                        onClick={() => {
+                            setModal(() => ({ isOpen: true, item: lesson.name }));
+                        }}
+                        className={styles.lesson}
+                        key={lesson.name}>
+                        <p className={styles.information__text}>
+                            <b>Предмет:</b> {lesson.name}
+                        </p>
+                        <div className={styles.progress__block}>
+                            <p className={styles.progress__text}>
+                                <b>Посещение:</b>
+                            </p>
+                            <Circle progress={lesson.attendance} />
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 
