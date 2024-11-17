1a963e14254856647185f71e2704a089f0952095
ssadown <bibatututu@yandex.ru>
2024-11-18 00:14:30 +0500

    Написал аутентифкацию

---
+++ src/Hooks/GetLogin.ts
@@ -0,0 +1,20 @@
+import { loginInterface } from "@/Interface/loginInterface";
+import axios from "axios";
+
+export const GetLogin = async (login: string, passwordLogin: string): Promise<loginInterface> => {
+    try {
+        const response = await axios.post('http://localhost:5007/api/account', {
+            email: login,
+            password: passwordLogin
+        });
+        console.log(response.data);
+        if (response.data) {
+            return response.data; 
+        } else {
+            throw new Error("Не удалось получить данные");
+        }
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
