ca174b6e466497dca08cb73bb5891013b8d9da96
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-19 23:21:42 +0500

    Изменил логику компонента auth: теперь там не используются стейты, а также useHandleLogin теперь полностью серверная функция

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,57 +1,45 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import styles from "./auth.module.css"
-import Input from '@/Components/Common/Input/Input';
-import Button from '@/Components/Common/Button/Button';
-import { useHandleLogin } from '@/Hooks/client/useHandleLogin';
+import React, { useActionState } from "react";
+import styles from "./auth.module.css";
+import Input from "@/Components/Common/Input/Input";
+import Button from "@/Components/Common/Button/Button";
+import { useHandleLogin } from "@/Hooks/client/useHandleLogin";
+import Form from "next/form";
 
-const Auth : React.FC = () => {
-    const [login, setLogin] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [error404, setError404] = useState<boolean>(false)
-    const [error401, setError401] = useState<boolean>(false)
-    const [error500, setError500] = useState<boolean>(false)
+const Auth: React.FC = () => {
+	/* error - это любое возвращенное значение из server actions  в нашем случае это значения из catch блока. Остальное и так понятно */
+	const [error, action, isPending] = useActionState(useHandleLogin, null);
+	return (
+		<main className={`main ${styles.auth__content}`}>
+			<h1 className={styles.auth__head_text}>Вход</h1>
+			<Form className={styles.auth__form} action={action}>
+				<Input
+					name={"login"}
+					type="text"
+					placeholder="Введите корп. почту"
+					size="m"
+				/>
+				<Input
+					name="password"
+					type="password"
+					placeholder="Введите пароль"
+					size="m"
+				/>
+				<Button disabled={isPending} text="Войти" size="s" />
+			</Form>
 
-    const handleChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLogin(e.target.value)
-    }    
-    const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-    }
-    const handleLogin = useHandleLogin({loginValue: login,password, setError404, setError401, setError500})
-    return (
-        <main className={`main ${styles.auth__content}`}>
-            <h1 className={styles.auth__head_text}>Вход</h1>
-            <Input
-                type='text'
-                value={login}
-                placeholder='Введите корп. почту'
-                onChange={handleChangeLogin}
-                size='m'
-            />
-            <Input
-                type="password"
-                value={password}
-                placeholder='Введите пароль'
-                onChange={handleChangePassword}
-                size='m'
-            />
-            <Button
-                text='Войти'
-                size="s"
-                onClick={handleLogin}
-            />
-            <p className={(error404 || error401 || error500) ? "error__text" : "error__text_hidden"}>
-                {
-                error404 ? "Такой учетной записи нет!" :
-                error401 ? "Вы неправильно ввели логин/пароль!" :
-                error500 ? "Сервер не работает, повторите попытку позже!" : ""
-                }
-            </p>
-
-        </main>
-    );
-}
+			<p className={error ? "error__text" : "error__text_hidden"}>
+				{error === "404"
+					? "Такой учетной записи нет!"
+					: error === "401"
+					? "Вы неправильно ввели логин/пароль!"
+					: error === "500"
+					? "Сервер не работает, повторите попытку позже!"
+					: ""}
+			</p>
+		</main>
+	);
+};
 
 export default Auth;
