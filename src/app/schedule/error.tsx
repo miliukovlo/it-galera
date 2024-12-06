294a87b9bea20a2cff039fd32f7d097b8c84ab88
ssadown <bibatututu@yandex.ru>
2024-12-06 20:48:30 +0500

    Подправил auth, middleware и создал error.tsx для schedule

---
+++ src/app/schedule/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            sorry server doesn`t work :(
+        </div>
+    );
+}
+
+export default Error;
